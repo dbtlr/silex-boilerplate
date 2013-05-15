bd9eeb953805af9a99cb4fe98b95383cd79adafb
Drew Butler <drew@nodrew.com>
2013-05-15 10:35:42 -0400

    Added backtrace method to the app.

--- src/Base/Application.php
+++ src/Base/Application.php
@@ -44,10 +44,46 @@ class Application extends SilexApplication
                 default:
                     $message = 'We are sorry, but something went terribly wrong.';
                     $message .= '<p>' . $e->getMessage() . '</p>';
-                    $message .= '<pre>' . backtrace($e->getTrace()) . '</pre>';
+                    $message .= '<pre>' . $app->backtrace($e->getTrace()) . '</pre>';
             }
 
             return new Response($message);
         });
+    }
+
+    /**
+     * Flatten the given debug_backtrace() array for easy printing. If none
+     * is given, one will be derived.
+     *
+     * @param array|null $backtrace
+     * @return string
+     */
+    public function backtrace($backtrace=null)
+    {
+        if (!isset($backtrace)) {
+            $backtrace = debug_backtrace();
+            array_shift($backtrace);
+        }
+
+        $backtraceOut = '';
+        $enableFor    = array('include', 'include_once', 'require', 'require_once');
+
+        foreach ($backtrace as $event) {
+            $arguments = '';
+
+            // disable argument output for all but the listed functions.
+            if ( in_array( $event['function'], $enableFor ) && isset ( $event['args'] ) ) {
+                $arguments = ArrayUtils::toArgString( $event['args'], true );
+            }
+
+            if (isset($event['file']) && isset($event['line'])) {
+                $backtraceOut .= 'File: ' . $event['file'] . ' Line: ' . $event['line'] . "\n";
+            }
+
+            $backtraceOut .= (isset($event['class']) ? $event['class'] . $event['type'] : '')
+                          . $event['function'] . '(' . $arguments . ")\n\n";
+        }
+
+        return trim($backtraceOut);
     }
 }
